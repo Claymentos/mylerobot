597e69cda2ba7bc3b2b2c59ae35fb52a75938bf2
Claymentos <clement.prigent@free.fr>
2026-07-09 11:44:04 +0200

    training script modification

--- train_act.sh
+++ train_act.sh
@@ -25,6 +25,7 @@ conda activate lerobot
 export WANDB_MODE=offline
 export WANDB_API_KEY=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
 export HF_HOME=$SCRATCH/.cache/huggingface
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 export HF_LEROBOT_HOME=$SCRATCH/.cache/huggingface/lerobot
 export HF_HUB_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
